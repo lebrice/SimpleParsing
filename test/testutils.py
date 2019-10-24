9138a00472baf8965de36ada8ef232dda2960d40
Fabrice Normandin <fabrice.normandin@gmail.com>
2019-10-24 13:57:43 -0400

    Updated testutils and test_bools
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>

--- test/testutils.py
+++ test/testutils.py
@@ -3,22 +3,55 @@ from typing import *
 import shlex
 import pytest
 import simple_parsing
-from simple_parsing import InconsistentArgumentError, ParseableFromCommandLine, Formatter
+from simple_parsing import InconsistentArgumentError, ArgumentParser, Formatter
 
+
+from simple_parsing.utils import camel_case
+
+T = TypeVar("T", bound="TestSetup")
 
 class TestSetup():
     @classmethod
-    def setup(cls, arguments: Optional[str] = "", multiple = False) -> argparse.Namespace:
-        parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-        # parser = argparse.ArgumentParser(formatter_class=Formatter)
-        cls.add_arguments(parser, multiple=multiple)
-        # BUG: the arguments might have quotes in them, hence we shouldn't necessarily just split() with whitespace..
+    def setup(cls: Type[T], arguments: Optional[str] = "", dest: Optional[str] = None) -> T:
+        """Basic setup for a test.
+        
+        Keyword Arguments:
+            arguments {Optional[str]} -- The arguments to pass to the parser (default: {""})
+            dest {Optional[str]} -- the attribute where the argument should be stored. (default: {None})
+        
+        Returns:
+            {cls}} -- the class's type.
+        """
+        parser = simple_parsing.ArgumentParser()
+        if dest is None:
+            dest = camel_case(cls.__name__)
+        
+        parser.add_arguments(cls, dest=dest)
+
         if arguments is None:
-            return parser.parse_args()
+            args = parser.parse_args()
         else:
             splits = shlex.split(arguments)
-            return parser.parse_args(splits)
+            args = parser.parse_args(splits)
+        instance: cls = getattr(parser, dest) #type: ignore
+        return instance
     
+    @classmethod
+    def setup_multiple(cls: Type[T], num_to_parse: int, arguments: Optional[str] = "") -> Tuple[T, ...]:
+        parser = simple_parsing.ArgumentParser()
+        class_name = camel_case(cls.__name__)
+        for i in range(num_to_parse):
+            parser.add_arguments(cls, f"{class_name}_{i}")
+
+        if arguments is None:
+            args = parser.parse_args()
+        else:
+            splits = shlex.split(arguments)
+            args = parser.parse_args(splits)
+
+        return tuple(getattr(args, f"{class_name}_{i}") for i in range(num_to_parse))
+        
+
     @classmethod
     def get_help_text(cls, multiple=False):
         import contextlib
