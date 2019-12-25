7276fc7d7bfc750972f38268ada0de89c733ff0a
Fabrice Normandin <fabrice.normandin@gmail.com>
2019-12-25 14:52:33 -0500

    Added Conditional dependency on dataclasses
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>

--- setup.py
+++ setup.py
@@ -3,10 +3,13 @@ import setuptools
 with open("README_pypi.md", "r") as fh:
     long_description = fh.read()
 print("PACKAGES FOUND:", setuptools.find_packages())
+import sys
+print(sys.version_info)
+print()
 
 setuptools.setup(
     name="simple_parsing",
-    version="0.0.3post4",
+    version="0.0.3.post5",
     author="Fabrice Normandin",
     author_email="fabrice.normandin@gmail.com",
     description="A small utility for simplifying and cleaning up argument parsing scripts.",
@@ -20,4 +23,5 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
+    install_requires=['dataclasses'] if sys.version_info < (3, 7) else [],
 )
