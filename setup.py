4f625c40146abf1a8087f0a7b7f8fcf66e61de5d
Fabrice <fabrice.normandin@gmail.com>
2019-12-27 18:35:29 -0500

    Revert "Added __version__ attribute to module"
    This reverts commit d4d1ade46a7a9f892de43dfc9e20ec864ae40560.

--- setup.py
+++ setup.py
@@ -4,11 +4,12 @@ with open("README_pypi.md", "r") as fh:
     long_description = fh.read()
 print("PACKAGES FOUND:", setuptools.find_packages())
 import sys
+print(sys.version_info)
+print()
 
-from simple_parsing import __version__
 setuptools.setup(
     name="simple_parsing",
-    version=__version__,
+    version="0.0.3.post6",
     author="Fabrice Normandin",
     author_email="fabrice.normandin@gmail.com",
     description="A small utility for simplifying and cleaning up argument parsing scripts.",
--- simple_parsing/__init__.py
+++ simple_parsing/__init__.py
@@ -4,8 +4,6 @@
 from .utils import Formatter, InconsistentArgumentError, MutableField, choice
 from .parsing import ArgumentParser, ConflictResolution
 
-__version__ = "0.0.3.post6"
-
 __all__ = [
     "ArgumentParser",
     "InconsistentArgumentError",
