8cf151430d464c59d4173c31ed090eb39d4e3414
Fabrice Normandin <fabrice.normandin@gmail.com>
2020-01-30 17:16:17 -0500

    [v0.0.6] Added Custom Argparse Options with field
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
+import sys
 import setuptools
 
 with open("README_pypi.md", "r") as fh:
     long_description = fh.read()
 packages = setuptools.find_namespace_packages(include=['simple_parsing*'])
 print("PACKAGES FOUND:", packages)
-import sys
 print(sys.version_info)
 
 setuptools.setup(
     name="simple_parsing",
-    version="0.0.5",
+    version="0.0.6",
     author="Fabrice Normandin",
     author_email="fabrice.normandin@gmail.com",
     description="A small utility for simplifying and cleaning up argument parsing scripts.",
