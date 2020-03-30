33a60f521beeced6bf4cb19e2fb7a81d91d920ca
Fabrice Normandin <fabrice.normandin@gmail.com>
2020-03-30 13:55:09 -0400

    Updated minor version to 0.0.9 - conflicts bugfix
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ print(sys.version_info)
 
 setuptools.setup(
     name="simple_parsing",
-    version="0.0.8.post1",
+    version="0.0.9",
     author="Fabrice Normandin",
     author_email="fabrice.normandin@gmail.com",
     description="A small utility for simplifying and cleaning up argument parsing scripts.",
