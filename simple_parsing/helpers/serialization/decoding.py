a3102fd74459f40c6363eb7fda6ca906c56f7752
Fabrice Normandin <fabrice.normandin@gmail.com>
2020-06-27 18:20:26 -0400

    Added support for decoding TypeVars
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>

--- simple_parsing/helpers/serialization/decoding.py
+++ simple_parsing/helpers/serialization/decoding.py
@@ -25,6 +25,7 @@ decoding_fns: Dict[Type, Callable[[Any], Any]] = {}
 
 def decode_optional(t: Type[T]) -> Callable[[Optional[Any]], Optional[T]]:
     decode = _get_decoding_fn(t)
+
     def _decode_optional(val: Optional[Any]) -> Optional[T]:
         return val if val is None else decode(val)
     return _decode_optional
@@ -51,7 +52,7 @@ def decode_union(*types: Type[T]) -> Callable[[Any], Union[T, Any]]:
     # Partion the Union into None and non-None types.
     while type(None) in types:
         types.remove(type(None))
-    
+
     decoding_fns: List[Callable[[Any], T]] = [
         decode_optional(t) if optional else _get_decoding_fn(t) for t in types
     ]
@@ -61,6 +62,7 @@ def decode_union(*types: Type[T]) -> Callable[[Any], Union[T, Any]]:
 
 def decode_list(t: Type[T]) -> Callable[[List[Any]], List[T]]:
     decode_item = _get_decoding_fn(t)
+
     def _decode_list(val: List[Any]) -> List[T]:
         return [decode_item(v) for v in val]
     return _decode_list
@@ -71,6 +73,7 @@ def decode_tuple(*tuple_item_types: Type[T]) -> Callable[[List[T]], Tuple[T, ...
     decoding_fns = [
         _get_decoding_fn(t) for t in tuple_item_types
     ]
+
     def _decode_tuple(val: Tuple[Any, ...]) -> Tuple[T, ...]:
         return tuple(
             decoding_fns[i](v) for i, v in enumerate(val)
@@ -80,6 +83,7 @@ def decode_tuple(*tuple_item_types: Type[T]) -> Callable[[List[T]], Tuple[T, ...
 
 def decode_set(t: Type[T]) -> Callable[[List[T]], Set[T]]:
     _decode = decode_list(t)
+
     def _decode_set(val: List[Any]) -> Set[T]:
         l = _decode(val)
         return set(l)
@@ -89,7 +93,8 @@ def decode_set(t: Type[T]) -> Callable[[List[T]], Set[T]]:
 def decode_dict(K_: Type[K], V_: Type[V]) -> Callable[[List[Tuple[Any, Any]]], Dict[K, V]]:
     decode_k = _get_decoding_fn(K_)
     decode_v = _get_decoding_fn(V_)
-    def _decode_dict(val: Union[Dict[Any, Any], List[Tuple[Any, Any]]]) -> Dict[K, V]:    
+
+    def _decode_dict(val: Union[Dict[Any, Any], List[Tuple[Any, Any]]]) -> Dict[K, V]:
         result: Dict[K, V] = {}
         if isinstance(val, list):
             result = OrderedDict()
@@ -116,26 +121,26 @@ def try_constructor(t: Type[T]) -> Callable[[Any], Union[T, Any]]:
 def _get_decoding_fn(t: Type[T]) -> Callable[[Any], T]:
     cache_info = _get_decoding_fn.cache_info()
     logger.debug(f"called for type {t}! Cache info: {cache_info}")
-   
+
     if t in decoding_fns:
         return decoding_fns[t]
     if t in {int, str, float, bool}:
         return t
-    
+
     from .serializable import Serializable
     if inspect.isclass(t) and issubclass(t, Serializable):
         return t.from_dict
     if t is Any:
         logger.debug(f"Decoding an Any type: {t}")
         return no_op
-    
+
     if is_dict(t):
         logger.debug(f"Decoding a Dict field: {t}")
         args = get_type_arguments(t)
         if len(args) != 2:
             args = (Any, Any)
         return decode_dict(*args)
-    
+
     if is_set(t):
         logger.debug(f"Decoding a Set field: {t}")
         args = get_type_arguments(t)
@@ -153,12 +158,12 @@ def _get_decoding_fn(t: Type[T]) -> Callable[[Any], T]:
         args = get_type_arguments(t)
         assert len(args) == 1
         return decode_list(args[0])
-    
+
     if is_union(t):
         logger.debug(f"Decoding a Union field: {t}")
         args = get_type_arguments(t)
         return decode_union(*args)
-        
+
     import typing_inspect as tpi
     from .serializable import get_dataclass_type_from_forward_ref
 
@@ -172,6 +177,13 @@ def _get_decoding_fn(t: Type[T]) -> Callable[[Any], T]:
             return partial(dc.from_dict, drop_extra_fields=False)
         if dc:
             return dc.from_dict
+
+    if tpi.is_typevar(t):
+        bound = tpi.get_bound(t)
+        logger.debug(f"Decoding a typevar: {t}, bound type is {bound}.")
+        if bound is not None:
+            return _get_decoding_fn(bound)
+        
 
     # Unknown type.
     warnings.warn(UserWarning(
--- test/test_decoding.py
+++ test/test_decoding.py
@@ -45,3 +45,57 @@ def test_encode_something(simple_attribute):
     #     "hey4": expected_value,
     # })
     assert SomeClass.loads(b.dumps()) == b
+
+
+
+def test_typevar_decoding(simple_attribute):
+    
+    @dataclass
+    class Item(Serializable, decode_into_subclasses=True):
+        name: str = "chair"
+        price: float = 399
+        stock: int = 10
+
+    @dataclass
+    class DiscountedItem(Item):
+        discount_factor: float = 0.5
+
+    I = TypeVar("I", bound=Item)
+
+    @dataclass
+    class Container(Serializable, Generic[I]):
+        items: List[I] = list_field()
+
+
+    chair = Item()
+    cheap_chair = DiscountedItem(name="Cheap chair")
+    c = Container(items=[chair, cheap_chair])
+
+    assert Container.loads(c.dumps()) == c
+
+
+
+    some_type, passed_value, expected_value = simple_attribute
+    @dataclass
+    class SomeClass(Serializable):
+        d: Dict[str, some_type] = dict_field()
+        l: List[Tuple[some_type, some_type]] = list_field()
+        t: Dict[str, Optional[some_type]] = dict_field()
+        # w: Dict[str, Union[some_type, int, str, None, str, None]] = dict_field()
+
+    b = SomeClass()
+    b.d.update({
+        "hey": expected_value
+    })
+    b.l.append((expected_value, expected_value))
+    b.t.update({
+        "hey": None,
+        "hey2": expected_value
+    })
+    # b.w.update({
+    #     "hey": None,
+    #     "hey2": "heyo",
+    #     "hey3": 1,
+    #     "hey4": expected_value,
+    # })
+    assert SomeClass.loads(b.dumps()) == b
