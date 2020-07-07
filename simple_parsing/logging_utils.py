e0661deac00f439a81023ac8cfdc207f21268c59
ivan <ivan.prado@gmail.com>
2020-07-07 12:33:24 +0100

    Remove call to logging.basicConfig in the module
    Otherwise code using this library will be forced to use this handler and
    it potentially will generate duplicated log lines in the output (as both
    the user and this library handlers are installed).
    
    basicConfig method should be only called by the library user, and never inside the
    module of the library itself

--- simple_parsing/logging_utils.py
+++ simple_parsing/logging_utils.py
@@ -1,12 +1,6 @@
 import logging
 from pathlib import Path
 
-logging.basicConfig(
-    format='%(levelname)-8s [%(name)s:%(lineno)d] %(message)s',
-    datefmt='%Y-%m-%d:%H:%M:%S',
-    level=logging.INFO,
-)
-
 def get_logger(name: str) -> logging.Logger:
         """ Gets a logger for the given file. Sets a nice default format. 
         TODO: figure out if we should add handlers, etc. 
