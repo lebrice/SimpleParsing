107e44b08d0833afdde15b0a09dfd42e6e3f6d7a
Fabrice Normandin <fabrice.normandin@gmail.com>
2020-07-31 12:21:09 -0400

    [v0.0.11.post14] Add dest to the option strings
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ print(sys.version_info)
 
 setuptools.setup(
     name="simple_parsing",
-    version="0.0.11.post13",
+    version="0.0.11.post14",
     author="Fabrice Normandin",
     author_email="fabrice.normandin@gmail.com",
     description="A small utility for simplifying and cleaning up argument parsing scripts.",
