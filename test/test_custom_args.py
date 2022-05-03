lebrice/SimpleParsing

commit 0092e52c2a38f0d647835293b9574cd7d27c8dc9

force_full_path option to use always full paths to fields as option string (#117)
* force_full_path option to use always full paths to fields as option string

* `add_dest_to_option_strings` removed in favour of `argument_generation_mode` and `nested_mode`

* Improved nested_mode doc

* Improve test_generation_mode

Co-authored-by: Fabrice Normandin <fabrice.normandin@gmail.com>

simple_parsing/wrappers/field_wrapper.py
@@ -2,7 +2,7 @@ import argparse
 import typing
 import dataclasses
 import inspect
-from enum import Enum
+from enum import Enum, auto
 from logging import getLogger
 from typing import cast, ClassVar, Any, Optional, List, Type, Dict, Set, Union, Tuple
 
@@ -19,6 +19,29 @@ if typing.TYPE_CHECKING:
     from .dataclass_wrapper import DataclassWrapper
 
 logger = getLogger(__name__)
+
+
+class ArgumentGenerationMode(Enum):
+    """
+    Enum for argument generation modes.
+    """
+    # Tries to generate flat arguments, removing the argument destination path when possible.
+    FLAT = auto()
+    # Generates arguments with their full destination path.
+    NESTED = auto()
+    # Generates both the flat and nested arguments.
+    BOTH = auto()
+
+
+class NestedMode(Enum):
+    """
+    Controls how nested arguments are generated.
+    """
+    # By default, the full destination path is used.
+    DEFAULT = auto()
+    # The full destination path is used, but the first level is removed.
+    # Useful because sometimes the first level is uninformative (i.e. 'args').
+    WITHOUT_ROOT = auto()
 
 
 class DashVariant(Enum):
@@ -65,8 +88,11 @@ class FieldWrapper(Wrapper[dataclasses.Field]):
     # TODO: This can often make "--help" messages a bit crowded
     add_dash_variants: ClassVar[DashVariant] = DashVariant.AUTO
 
-    # Whether to add the `dest` to the list of option strings.
-    add_dest_to_option_strings: ClassVar[bool] = True
+    # Whether to follow a flat or nested argument structure.
+    argument_generation_mode: ClassVar[ArgumentGenerationMode] = ArgumentGenerationMode.FLAT
+
+    # Controls how nested arguments are generated.
+    nested_mode: ClassVar[NestedMode] = NestedMode.DEFAULT
 
     def __init__(self, field: dataclasses.Field, parent: Any = None, prefix: str = ""):
         super().__init__(wrapped=field, name=field.name)
@@ -517,11 +543,20 @@ class FieldWrapper(Wrapper[dataclasses.Field]):
         dashes: List[str] = []  # contains the leading dashes.
         options: List[str] = []  # contains the name following the dashes.
 
+        def add_args(dash:str, candidates: List[str]) -> None:
+            for candidate in candidates:
+                options.append(candidate)
+                dashes.append(dash)
+
         # Handle user passing us "True" or "only" directly.
         add_dash_variants = DashVariant(FieldWrapper.add_dash_variants)
 
+        gen_mode = type(self).argument_generation_mode
+        nested_mode = type(self).nested_mode
+
         dash = "-" if len(self.name) == 1 else "--"
         option = f"{self.prefix}{self.name}"
+        nested_option = self.dest if nested_mode == NestedMode.DEFAULT else ".".join(self.dest.split(".")[1:])
         if add_dash_variants == DashVariant.DASH:
             option = option.replace("_", "-")
 
@@ -529,13 +564,18 @@ class FieldWrapper(Wrapper[dataclasses.Field]):
             # Can't be positional AND have flags at same time. Also, need dest to be be this and not just option.
             return [self.dest]
 
-        dashes.append(dash)
-        options.append(option)
+        if gen_mode == ArgumentGenerationMode.FLAT:
+            candidates = [option]
+        elif gen_mode == ArgumentGenerationMode.NESTED:
+            candidates = [nested_option]
+        else:
+            candidates = [option, nested_option]
+
+        add_args(dash, candidates)
 
         if dash == "-":
             # also add a double-dash option:
-            dashes.append("--")
-            options.append(option)
+            add_args("--", candidates)
 
         # add all the aliases that were passed to the `field` function.
         for alias in self.aliases:
@@ -566,10 +606,6 @@ class FieldWrapper(Wrapper[dataclasses.Field]):
             ]
             options.extend(additional_options)
             dashes.extend(additional_dashes)
-
-        if type(self).add_dest_to_option_strings:
-            dashes.append("-" if len(self.dest) == 1 else "--")
-            options.append(self.dest)
 
         # remove duplicates by creating a set.
         option_strings = set(f"{dash}{option}" for dash, option in zip(dashes, options))

examples/simple/option_strings.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 from simple_parsing import ArgumentParser, choice, field
+from simple_parsing.wrappers.field_wrapper import ArgumentGenerationMode
 
 
 @dataclass
@@ -42,7 +43,7 @@ HParams ['hparams']:
 """
 
 # Now if we wanted to also be able to set the arguments using their full paths:
-parser = ArgumentParser(add_dest_to_option_strings=True)
+parser = ArgumentParser(argument_generation_mode=ArgumentGenerationMode.BOTH)
 parser.add_arguments(HParams, dest="hparams")
 parser.print_help()
 expected += """

simple_parsing/parsing.py
@@ -13,6 +13,7 @@ from .conflicts import ConflictResolution, ConflictResolver
 from .help_formatter import SimpleHelpFormatter
 from .utils import Dataclass
 from .wrappers import DataclassWrapper, FieldWrapper, DashVariant
+from .wrappers.field_wrapper import ArgumentGenerationMode, NestedMode
 
 logger = getLogger(__name__)
 
@@ -27,7 +28,8 @@ class ArgumentParser(argparse.ArgumentParser):
         *args,
         conflict_resolution: ConflictResolution = ConflictResolution.AUTO,
         add_option_string_dash_variants: DashVariant = DashVariant.AUTO,
-        add_dest_to_option_strings: bool = False,
+        argument_generation_mode = ArgumentGenerationMode.FLAT,
+        nested_mode: NestedMode = NestedMode.DEFAULT,
         formatter_class: Type[HelpFormatter] = SimpleHelpFormatter,
         **kwargs,
     ):
@@ -49,20 +51,34 @@ class ArgumentParser(argparse.ArgumentParser):
             "--no-cache" and "--no_cache" can both be used to point to the same
             attribute `no_cache` on some dataclass.
 
-        - add_dest_to_option_strings: bool, optional
+        - argument_generation_mode : ArgumentGenerationMode, optional
 
-            Whether or not to add the `dest` of each field to the list of option
-            strings for the argument.
-            When True (default), each field can be referenced using either the
-            auto-generated option string or the full 'destination' of the field
-            in the resulting namespace.
-            When False, only uses the auto-generated option strings.
+            How to generate the arguments. In the ArgumentGenerationMode.FLAT mode,
+            the default one, the arguments are flat when possible, ignoring
+            their nested structure and including it only on the presence of a
+            conflict.
 
-            The auto-generated option strings are usually just the field names,
-            except when there are multiple arguments with the same name. In this
-            case, the conflicts are resolved as determined by the value of
-            `conflict_resolution` and each field ends up with a unique option
-            string.
+            In the ArgumentGenerationMode.NESTED mode, the arguments are always
+            composed reflecting their nested structure.
+
+            In the ArgumentGenerationMode.BOTH mode, both kind of arguments
+            are generated.
+
+        - nested_mode : NestedMode, optional
+
+            How to handle argument generation in for nested arguments
+            in the modes ArgumentGenerationMode.NESTED and ArgumentGenerationMode.BOTH.
+            In the NestedMode.DEFAULT mode, the nested arguments are generated
+            reflecting their full 'destination' path from the returning namespace.
+
+            In the NestedMode.WITHOUT_ROOT, the first level is removed. This is useful when
+            parser.add_arguments is only called once, and where the same prefix would be shared
+            by all arguments. For example, if you have a single dataclass MyArguments and
+            you call parser.add_arguments(MyArguments, "args"), the arguments could look like this:
+            '--args.input.path --args.output.path'.
+            We could prefer to remove the root level in such a case
+             so that the arguments get generated as
+            '--input.path --output.path'.
 
         - formatter_class : Type[HelpFormatter], optional
 
@@ -85,7 +101,8 @@ class ArgumentParser(argparse.ArgumentParser):
 
         self._preprocessing_done: bool = False
         FieldWrapper.add_dash_variants = add_option_string_dash_variants
-        FieldWrapper.add_dest_to_option_strings = add_dest_to_option_strings
+        FieldWrapper.argument_generation_mode = argument_generation_mode
+        FieldWrapper.nested_mode = nested_mode
 
     @overload
     def add_arguments(

test/test_generation_mode.py
@@ -1,0 +1,77 @@
+from dataclasses import dataclass
+
+import pytest
+
+from simple_parsing.wrappers.field_wrapper import ArgumentGenerationMode, NestedMode
+from . import TestSetup
+
+
+@dataclass
+class ModelOptions:
+  path: str
+  device: str
+
+
+@dataclass
+class ServerOptions(TestSetup):
+  host: str
+  port: int
+  model: ModelOptions
+
+
+expected = ServerOptions(host="myserver", port=80, model=ModelOptions(path="a_path", device="cpu"))
+
+
+def test_flat():
+      options = ServerOptions.setup(
+          "--host myserver " "--port 80 " "--path a_path " "--device cpu",
+      )
+      assert options == expected
+
+      with pytest.raises(SystemExit):
+          ServerOptions.setup(
+              "--opts.host myserver " "--opts.port 80 " "--opts.model.path a_path " "--opts.model.device cpu",
+              dest="opts"
+          )
+
+
+@pytest.mark.parametrize("without_root", [True, False])
+def test_both(without_root):
+    options = ServerOptions.setup(
+        "--host myserver " "--port 80 " "--path a_path " "--device cpu",
+        dest="opts",
+        argument_generation_mode=ArgumentGenerationMode.BOTH
+    )
+    assert options == expected
+
+    args = "--opts.host myserver " "--opts.port 80 " "--opts.model.path a_path " "--opts.model.device cpu"
+    if without_root:
+        args = args.replace("opts.", "")
+    options = ServerOptions.setup(
+        args,
+        dest="opts",
+        argument_generation_mode=ArgumentGenerationMode.BOTH,
+        nested_mode=NestedMode.WITHOUT_ROOT if without_root else NestedMode.DEFAULT,
+    )
+    assert options == expected
+
+
+@pytest.mark.parametrize("without_root", [True, False])
+def test_nested(without_root):
+    with pytest.raises(SystemExit):
+        options = ServerOptions.setup(
+            "--host myserver " "--port 80 " "--path a_path " "--device cpu",
+            dest="opts",
+            argument_generation_mode=ArgumentGenerationMode.NESTED
+        )
+
+    args = "--opts.host myserver " "--opts.port 80 " "--opts.model.path a_path " "--opts.model.device cpu"
+    if without_root:
+        args = args.replace("opts.", "")
+    options = ServerOptions.setup(
+        args,
+        dest="opts",
+        argument_generation_mode=ArgumentGenerationMode.NESTED,
+        nested_mode=NestedMode.WITHOUT_ROOT if without_root else NestedMode.DEFAULT,
+    )
+    assert options == expected

test/test_issue_46.py
@@ -1,6 +1,7 @@
 from io import StringIO
 from dataclasses import dataclass
 import simple_parsing
+from simple_parsing.wrappers.field_wrapper import ArgumentGenerationMode
 import textwrap
 import pytest
 
@@ -75,7 +76,7 @@ def test_issue_46(assert_equals_stdout):
 
 def test_issue_46_solution2(assert_equals_stdout):
     # This (now) works:
-    parser = simple_parsing.ArgumentParser(add_dest_to_option_strings=True)
+    parser = simple_parsing.ArgumentParser(argument_generation_mode=ArgumentGenerationMode.BOTH)
     parser.add_argument("--run_id", type=str)
     parser.add_arguments(JBuildRelease, dest="jbuild", prefix="jbuild.")
     s = StringIO()

test/testutils.py
@@ -30,6 +30,7 @@ from simple_parsing import (
 )
 from simple_parsing.utils import camel_case
 from simple_parsing.wrappers import DataclassWrapper
+from simple_parsing.wrappers.field_wrapper import ArgumentGenerationMode, NestedMode
 
 xfail = pytest.mark.xfail
 parametrize = pytest.mark.parametrize
@@ -142,6 +143,10 @@ class TestSetup:
         add_option_string_dash_variants: DashVariant = DashVariant.AUTO,
         parse_known_args: bool = False,
         attempt_to_reorder: bool = False,
+        *,
+        argument_generation_mode: ArgumentGenerationMode = ArgumentGenerationMode.FLAT,
+        nested_mode: NestedMode = NestedMode.DEFAULT,
+
     ) -> Dataclass:
         """Basic setup for a test.
 
@@ -155,6 +160,8 @@ class TestSetup:
         parser = simple_parsing.ArgumentParser(
             conflict_resolution=conflict_resolution_mode,
             add_option_string_dash_variants=add_option_string_dash_variants,
+            argument_generation_mode=argument_generation_mode,
+            nested_mode=nested_mode,
         )
         if dest is None:
             dest = camel_case(cls.__name__)

commit 009a6013be2c72547116cfc3cfeb9e360d31ebc5

Fix a bug introduced in #117. It affects the DASH option in the nested mode (#129)
* force_full_path option to use always full paths to fields as option string

* `add_dest_to_option_strings` removed in favour of `argument_generation_mode` and `nested_mode`

* Improved nested_mode doc

* Improve test_generation_mode

* Fix a bug introduced in https://github.com/lebrice/SimpleParsing/pull/117. It affects the DASH option in the nested mode.

* Including a test case

Co-authored-by: Fabrice Normandin <fabrice.normandin@gmail.com>

simple_parsing/wrappers/field_wrapper.py
@@ -559,6 +559,7 @@ class FieldWrapper(Wrapper[dataclasses.Field]):
         nested_option = self.dest if nested_mode == NestedMode.DEFAULT else ".".join(self.dest.split(".")[1:])
         if add_dash_variants == DashVariant.DASH:
             option = option.replace("_", "-")
+            nested_option = nested_option.replace("_", "-")
 
         if self.field.metadata.get("positional"):
             # Can't be positional AND have flags at same time. Also, need dest to be be this and not just option.

test/test_custom_args.py
@@ -165,29 +165,55 @@ def test_store_false_action():
 
 def test_only_dashes():
     @dataclass
+    class AClass(TestSetup):
+        """foo"""
+
+        a_var: int
+
+
+    @dataclass
     class SomeClass(TestSetup):
         """lol"""
 
         my_var: int
+        a: AClass
 
     assert_help_output_equals(
         SomeClass.get_help_text(add_option_string_dash_variants=DashVariant.DASH),
         textwrap.dedent(
             """\
-            usage: pytest [-h] --my-var int
+            usage: pytest [-h] --my-var int --a-var int
 
             optional arguments:
-            -h, --help            show this help message and exit
-
+              -h, --help    show this help message and exit
+            
             test_only_dashes.<locals>.SomeClass ['some_class']:
-            lol
-
-            --my-var int
+              lol
+            
+              --my-var int
+            
+            test_only_dashes.<locals>.AClass ['some_class.a']:
+              foo
+            
+              --a-var int
             """
         ),
     )
-    sc = SomeClass.setup("--my-var 2", add_option_string_dash_variants=DashVariant.DASH)
+    sc = SomeClass.setup("--my-var 2 --a-var 3", add_option_string_dash_variants=DashVariant.DASH)
     assert sc.my_var == 2
+    assert sc.a.a_var == 3
+    sc = SomeClass.setup("--some-class.my-var 2 --some-class.a.a-var 3",
+                         add_option_string_dash_variants=DashVariant.DASH,
+                         argument_generation_mode=ArgumentGenerationMode.NESTED)
+    assert sc.my_var == 2
+    assert sc.a.a_var == 3
+    sc = SomeClass.setup("--my-var 2 --a.a-var 3",
+                         add_option_string_dash_variants=DashVariant.DASH,
+                         argument_generation_mode=ArgumentGenerationMode.NESTED,
+                         nested_mode=NestedMode.WITHOUT_ROOT)
+    assert sc.my_var == 2
+    assert sc.a.a_var == 3
+
 
 
 def test_list_of_choices():