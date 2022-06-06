2e669e0aabb2aca39ec137c99c577bf037611e39
Fabrice Normandin <fabrice.normandin@gmail.com>
2022-06-06 13:46:44 -0400

    Improved tests for issue 144
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>

--- test/test_issue_144.py
+++ test/test_issue_144.py
@@ -1,37 +1,69 @@
 """ Tests for issue 144: https://github.com/lebrice/SimpleParsing/issues/144 """
 from __future__ import annotations
 
-from dataclasses import dataclass
-from typing import Optional, Union
+from dataclasses import dataclass, field
+from typing import Dict, List, Optional, Tuple, Union
+
+import pytest
 
 from simple_parsing.helpers.serialization.serializable import Serializable
 
 
-@dataclass
-class Foo(Serializable):
-    bar: Optional[int] = 123
-
-
 class TestOptional:
-    def test_deserialize(self):
-        foo = Foo.from_dict({"bar": None})
-        assert foo == Foo(bar=None)
+    @dataclass
+    class Foo(Serializable):
+        foo: Optional[int] = 123
 
-    def test_serialize(self):
-        foo = Foo.from_dict({"bar": None})
-        assert foo == Foo(bar=None)
-
-
-@dataclass
-class Bar(Serializable):
-    a: Union[int, dict[int, bool]] = 123
+    @pytest.mark.parametrize("d", [{"foo": None}, {"foo": 1}])
+    def test_round_trip(self, d: dict):
+        # NOTE: this double round-trip makes the comparison agnostic to any conversion that may
+        # happen between the raw dict values and the arguments of the dataclasses.
+        assert self.Foo.from_dict(self.Foo.from_dict(d).to_dict()) == self.Foo.from_dict(d)
 
 
 class TestUnion:
-    def test_deserialize(self):
-        assert Bar.from_dict({"a": 1}) == Bar(a=1)
-        assert Bar.from_dict({"a": {1: True}}) == Bar(a={1: True})
+    @dataclass
+    class Foo(Serializable):
+        foo: Union[int, dict[int, bool]] = 123
 
-    def test_serialize(self):
-        assert Bar.from_dict({"a": 1}) == Bar(a=1)
-        assert Bar.from_dict({"a": {1: True}}) == Bar(a={1: True})
+    @pytest.mark.parametrize("d", [{"foo": None}, {"foo": {1: "False"}}])
+    def test_round_trip(self, d: dict):
+        # NOTE: this double round-trip makes the comparison agnostic to any conversion that may
+        # happen between the raw dict values and the arguments of the dataclasses.
+        assert self.Foo.from_dict(self.Foo.from_dict(d).to_dict()) == self.Foo.from_dict(d)
+
+
+class TestList:
+    @dataclass
+    class Foo(Serializable):
+        foo: List[int] = field(default_factory=list)
+
+    @pytest.mark.parametrize("d", [{"foo": []}, {"foo": [123, 456]}])
+    def test_round_trip(self, d: dict):
+        # NOTE: this double round-trip makes the comparison agnostic to any conversion that may
+        # happen between the raw dict values and the arguments of the dataclasses.
+        assert self.Foo.from_dict(self.Foo.from_dict(d).to_dict()) == self.Foo.from_dict(d)
+
+
+class TestTuple:
+    @dataclass
+    class Foo(Serializable):
+        foo: Tuple[int, float, bool]
+
+    @pytest.mark.parametrize("d", [{"foo": (1, 1.2, False)}, {"foo": ("1", "1.2", "True")}])
+    def test_round_trip(self, d: dict):
+        # NOTE: this double round-trip makes the comparison agnostic to any conversion that may
+        # happen between the raw dict values and the arguments of the dataclasses.
+        assert self.Foo.from_dict(self.Foo.from_dict(d).to_dict()) == self.Foo.from_dict(d)
+
+
+class TestDict:
+    @dataclass
+    class Foo(Serializable):
+        foo: Dict[int, float] = field(default_factory=dict)
+
+    @pytest.mark.parametrize("d", [{"foo": {}}, {"foo": {"123": "4.56"}}])
+    def test_round_trip(self, d: dict):
+        # NOTE: this double round-trip makes the comparison agnostic to any conversion that may
+        # happen between the raw dict values and the arguments of the dataclasses.
+        assert self.Foo.from_dict(self.Foo.from_dict(d).to_dict()) == self.Foo.from_dict(d)
