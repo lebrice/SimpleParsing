95373b39f4f3d5800be1c6fcf892cc3fe2613f09
Conchylicultor <etiennefg.pot@gmail.com>
2023-01-06 17:54:30 +0100

    Better bool parsing

--- test/test_bools.py
+++ test/test_bools.py
@@ -1,5 +1,8 @@
 from dataclasses import dataclass
 
+import pytest
+
+from simple_parsing import helpers
 from .testutils import TestSetup
 
 
@@ -18,26 +21,81 @@ class Flags(TestSetup):
     c: bool = False  # optional flag 'c'.
 
 
-def test_bool_attributes_work():
-    ext = Base.setup("--a 5 --f")
-    assert ext.f is True
+@pytest.mark.parametrize(
+    "flag,f",
+    [
+        ("--a 5", False),
+        ("--a 5 --f", True),
+        ("--a 5 --nof", False),
+        ("--a 5 --f=true", True),
+        ("--a 5 --f true", True),
+        ("--a 5 --f True", True),
+        ("--a 5 --f=false", False),
+        ("--a 5 --f false", False),
+        ("--a 5 --f False", False),
+        ("--a 5 --f --f false", False),
+    ],
+)
+def test_bool_base_work(flag, f):
+    ext = Base.setup(flag)
+    assert ext.f is f
 
-    ext = Base.setup("--a 5")
-    assert ext.f is False
 
-    true_strings = ["True", "true"]
-    for s in true_strings:
-        ext = Base.setup(f"--a 5 --f {s}")
-        assert ext.f is True
+@pytest.mark.parametrize(
+    "flag,a,b,c",
+    [
+        ("--a", True, True, False),
+        ("--a true --b --c", True, True, True),
+        ("--a true --noc --b --c --noc", True, True, False),
+        ("--noa --b false --noc", False, False, False),
+    ],
+)
+def test_bool_flags_work(flag, a, b, c):
+    flags = Flags.setup(flag)
+    assert flags.a is a
+    assert flags.b is b
+    assert flags.c is c
 
-    false_strings = ["False", "false"]
-    for s in false_strings:
-        ext = Base.setup(f"--a 5 --f {s}")
-        assert ext.f is False
 
+@pytest.mark.parametrize(
+    "flag, nargs, a",
+    [
+        # By default, support both --noflag and --flag=false
+        ("--a", None, True),
+        ("--noa", None, False),
+        ("--a true", None, True),
+        ("--a true false", None, SystemExit),
+        # 1 argument explicitly required
+        ("--a", 1, SystemExit),
+        ("--noa", 1, SystemExit),
+        ("--a=true", 1, [True]),
+        ("--a true false", 1, SystemExit),
+        # 2 argument explicitly required
+        ("--a", 2, SystemExit),
+        ("--noa", 2, SystemExit),
+        ("--a=true", 2, SystemExit),
+        ("--a true false", 2, [True, False]),
+        # 1+ argument explicitly required
+        ("--a", '+', SystemExit),
+        ("--noa", '+', SystemExit),
+        ("--a=true", '+', [True]),
+        ("--a true false", '+', [True, False]),
+        # 0 or 1+ argument explicitly required
+        ("--a", '*', [True]),
+        ("--noa", '*', [False]),
+        ("--a=true", '*', [True]),
+        ("--a true false", '*', [True, False]),
+    ],
+)
+def test_bool_nargs(flag, nargs, a):
+    @dataclass
+    class MyClass(TestSetup):
+        """Some extension of base-class `Base`"""
+        a: bool = helpers.field(nargs=nargs)
 
-def test_bool_flags_work():
-    flags = Flags.setup("--a true --b --c")
-    assert flags.a is True
-    assert flags.b is False
-    assert flags.c is True
+    if a == SystemExit:
+        with pytest.raises(SystemExit):
+            MyClass.setup(flag)
+    else:
+        flags = MyClass.setup(flag)
+        assert flags.a == a
