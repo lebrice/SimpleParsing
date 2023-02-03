e06154cb7c47b99509c52412ad66aa8bfec2c382
Fabrice Normandin <fabrice.normandin@gmail.com>
2023-02-03 15:03:18 -0500

    Fix error handling issues with `replace` function
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>

--- simple_parsing/replace.py
+++ simple_parsing/replace.py
@@ -17,33 +17,49 @@ def replace(obj: DataclassT, **changes) -> DataclassT:
 
 
 def replace(obj: DataclassT, changes_dict: dict[str, Any] | None = None, **changes) -> DataclassT:
-    """Replace some values in a dataclass. Also works with nested fields of the dataclass.
-    This is essentially an extension of `dataclasses.replace` that also allows changing the nested
-    fields of dataclasses.
+    """Replace some values in a dataclass.
+
+    Compared to `dataclasses.replace`, this has two major differences:
+    1. Allows recursively replacing values in nested dataclass fields. When a dictionary is passed
+    as the value, and the value of that field on `obj` is a dataclass, then `replace` is called
+    recursively.
+
+    2. Allows passing a dictionary of flattened changes, e.g. `{"a.b": 1}` instead of
+    `{"a": {"b": 1}}`.
 
     ## Examples
-    >>> from dataclasses import dataclass, field
+
+    >>> import dataclasses
+    >>> import simple_parsing
     >>> from typing import Union
-    >>> @dataclass
+    >>> @dataclasses.dataclass
     ... class A:
     ...    a: int = 0
-    >>> @dataclass
+    >>> @dataclasses.dataclass
     ... class B:
     ...     b: str = "b"
-    >>> @dataclass
+    >>> @dataclasses.dataclass
     ... class Config:
-    ...     a_or_b: Union[A, B] = field(default_factory=A)
+    ...     a_or_b: Union[A, B] = dataclasses.field(default_factory=A)
 
-    >>> config = Config(a_or_b=A(a=1))
+    >>> base_config = Config(a_or_b=A(a=1))
+
+
+    NOTE: key difference with respect to `dataclasses.replace`:
+    >>> dataclasses.replace(base_config, a_or_b={"a": 123})
+    Config(a_or_b={'a': 123})
+    >>> simple_parsing.replace(base_config, a_or_b={"a": 123})
+    Config(a_or_b=A(a=123))
 
     Replace accepts either a dictionary of changes or keyword arguments:
-    >>> replace(config, {"a_or_b": B(b="bob")})
+
+    >>> simple_parsing.replace(base_config, {"a_or_b": B(b="bob")})
     Config(a_or_b=B(b='bob'))
-    >>> replace(config, a_or_b=B(b='bob'))
+    >>> simple_parsing.replace(base_config, a_or_b=B(b='bob'))
     Config(a_or_b=B(b='bob'))
 
     Changes can also be passed in a 'flat' format, which makes it easy to replace nested fields:
-    >>> replace(config, {"a_or_b.a": 2})
+    >>> simple_parsing.replace(base_config, {"a_or_b.a": 2})
     Config(a_or_b=A(a=2))
     """
     if changes_dict and changes:
@@ -58,15 +74,19 @@ def replace(obj: DataclassT, changes_dict: dict[str, Any] | None = None, **chang
         if field.name not in changes:
             continue
         if not field.init:
-            raise ValueError(
-                f"Cannot replace value of non-init field {field.name}.")
+            raise ValueError(f"Cannot replace value of non-init field {field.name}.")
 
         field_value = getattr(obj, field.name)
 
         if is_dataclass_instance(field_value) and isinstance(changes[field.name], dict):
-            field_changes = changes[field.name]
+            field_changes = changes.pop(field.name)
             new_value = replace(field_value, **field_changes)
         else:
-            new_value = changes[field.name]
+            new_value = changes.pop(field.name)
         replace_kwargs[field.name] = new_value
+
+    # note: there may be some leftover values in `changes` that are not fields of this dataclass.
+    # we still pass those.
+    replace_kwargs.update(changes)
+
     return dataclasses.replace(obj, **replace_kwargs)
