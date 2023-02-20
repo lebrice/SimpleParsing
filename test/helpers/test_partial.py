dff9d350557fbd56d038db75c8f2c3e30e0d27ab
Fabrice Normandin <fabrice.normandin@gmail.com>
2023-02-20 18:41:42 -0500

    Adding some more tests for Partial
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>

--- test/helpers/test_partial.py
+++ test/helpers/test_partial.py
@@ -1,9 +1,10 @@
-from dataclasses import dataclass, is_dataclass
+import functools
+from dataclasses import dataclass, fields, is_dataclass
 
 from simple_parsing import ArgumentParser
 from simple_parsing.helpers.partial import Partial
 
-from .testutils import TestSetup
+from ..testutils import TestSetup
 
 
 @dataclass
@@ -27,16 +28,20 @@ def test_partial_class_attribute():
     args = parser.parse_args("--a 456".split())
     bob = args.bob
     foo_factory: Partial[Foo] = bob.foo_factory
-    assert is_dataclass(foo_factory)
     assert foo_factory.a == 456
     assert foo_factory.b == 2
     assert str(foo_factory) == "FooConfig(a=456, b=2)"
+
+    foo = foo_factory()
+    assert foo == Foo(a=456, b=2)
+    assert is_dataclass(foo_factory)
+    assert isinstance(foo_factory, functools.partial)
 
 
 def test_partial_function_attribute():
     @dataclass
     class Bob(TestSetup):
-        some_fn: Partial[some_function]
+        some_fn: Partial[some_function]  # type: ignore
 
     bob = Bob.setup("--v2 781")
     assert str(bob.some_fn) == "some_function_config(v1=123, v2=781)"
@@ -46,9 +51,6 @@ def test_partial_function_attribute():
 
 def test_dynamic_classes_are_cached():
     assert Partial[Foo] is Partial[Foo]
-
-
-# bob = Bob(foo_factory=Foo, some_fn=some_function)
 
 
 def test_pickling():
@@ -62,3 +64,29 @@ def test_pickling():
 
     deserialized = pickle.loads(serialized)
     assert deserialized is dynamic_class
+
+
+def some_function_with_required_arg(required_arg, v1: int = 123, v2: int = 456):
+    """Gives back the mean of two numbers."""
+    return required_arg, (v1 + v2) / 2
+
+
+@dataclass
+class FooWithRequiredArg(TestSetup):
+    some_fn: Partial[some_function_with_required_arg]
+
+
+def test_partial_for_fn_with_required_args():
+    bob = FooWithRequiredArg.setup("--v1 1 --v2 2")
+    assert is_dataclass(bob.some_fn)
+    assert isinstance(bob.some_fn, functools.partial)
+
+    assert "required_arg" not in [f.name for f in fields(bob.some_fn)]
+    assert bob.some_fn(123) == (123, 1.5)
+
+
+def test_getattr():
+    bob = FooWithRequiredArg.setup("--v1 1 --v2 2")
+    some_fn_partial = bob.some_fn
+    assert some_fn_partial.v1 == 1
+    assert some_fn_partial.v2 == 2
