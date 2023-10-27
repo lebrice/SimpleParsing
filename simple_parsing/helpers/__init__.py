45bd7c03fdadfda24e04e3a9d058eae65408335e
Fabrice Normandin <fabrice.normandin@gmail.com>
2023-10-27 11:41:28 -0400

    Fix issues with * import in helpers module
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>

--- simple_parsing/helpers/__init__.py
+++ simple_parsing/helpers/__init__.py
@@ -1,5 +1,15 @@
 """Collection of helper classes and functions to reduce boilerplate code."""
-from .fields import *
+from .fields import (
+    choice,
+    dict_field,
+    field,
+    flag,
+    flags,
+    list_field,
+    mutable_field,
+    set_field,
+    subparsers,
+)
 from .flatten import FlattenedAccess
 from .hparams import HyperParameters
 from .partial import Partial, config_for
@@ -13,3 +23,26 @@ except ImportError:
 # For backward compatibility purposes
 JsonSerializable = Serializable
 SimpleEncoder = SimpleJsonEncoder
+
+__all__ = [
+    "FlattenedAccess",
+    "HyperParameters",
+    "Partial",
+    "config_for",
+    "FrozenSerializable",
+    "Serializable",
+    "SimpleJsonEncoder",
+    "encode",
+    "JsonSerializable",
+    "SimpleEncoder",
+    "YamlSerializable",
+    "field",
+    "choice",
+    "list_field",
+    "dict_field",
+    "set_field",
+    "mutable_field",
+    "subparsers",
+    "flag",
+    "flags",
+]
